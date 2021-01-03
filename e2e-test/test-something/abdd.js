39294a3420518bbd933a79e18c7db8c22696c53e
xywang68 <xywang68@gmail.com>
2021-01-02 23:03:53 -0800

    add baseUrl for module

--- e2e-test/test-something/abdd.js
+++ e2e-test/test-something/abdd.js
@@ -1,4 +1,5 @@
 require('./support/env.js');
 var moduleAbdd = require(`${process.env.PROJECTRUNPATH}/${process.env.TestDir}/support/abdd.js`);
 // modify or add myAbdd attributes as necessary
+moduleAbdd.baseUrl = 'chrome://version';
 module.exports = moduleAbdd;
