766a944a42dced0f73b7cb61e67b6d29bb9b4b54
xywang68 <xywang68@gmail.com>
2021-02-24 02:16:19 -0800

    prunner ready

--- e2e-test/test-something/abdd.js
+++ e2e-test/test-something/abdd.js
@@ -1,4 +1,13 @@
-require('./support/env.js');
+const path = require('path');
+const PROJECTBASE = process.env.PROJECTBASE || 'test-projects';
+const PROJECTNAME = process.env.PROJECTNAME || path.resolve().split(PROJECTBASE)[1].split('/')[1];
+var moduleDepth = path.resolve(__dirname).split(PROJECTNAME)[1].split('/').length - 2;
+var relativePathToProject = '../'.repeat(moduleDepth);
+// define module level Env vars here
+process.env.TestDir = path.resolve(__dirname).split(PROJECTNAME)[1].split('/')[1];
+process.env.TestModule = path.resolve(__dirname).split(PROJECTNAME)[1].split('/')[2];
+require(relativePathToProject + 'support/env.js');
+require(path.resolve(__dirname) + '/support/env.js');
 var moduleAbdd = require(`${process.env.PROJECTRUNPATH}/${process.env.TestDir}/support/abdd.js`);
 // modify or add myAbdd attributes as necessary
 moduleAbdd.baseUrl = 'chrome://version';
